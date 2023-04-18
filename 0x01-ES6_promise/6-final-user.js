e0c74d534b9edda73188786bd91ef421e74255dd
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-04-18 23:45:06 +0300

    Write function to return array when promises are settled

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUp = signUpUser(firstName, lastName);
+  const upload = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUp, upload])
+    .then((values) => {
+      const res = [];
+      values.forEach((item) => {
+        if (item.status === 'fulfilled') {
+          res.push({ status: item.status, value: item.value });
+        } else {
+          res.push({ status: item.status, value: item.reason });
+        }
+      });
+    });
+}
