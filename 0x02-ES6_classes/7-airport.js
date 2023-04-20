78bc24bc8c213377178463fb673241b295093921
Sn0wF1re <mokayaclinton34@gmail.com>
2023-04-20 16:11:20 +0300

    Implement class named Airport that has a default string

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,14 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name === 'string') {
+      this._name = name;
+    }
+    if (typeof code === 'string') {
+      this._code = code;
+    }
+  }
+
+  toString() {
+    return (`[object ${this._code}]`);
+  }
+}
