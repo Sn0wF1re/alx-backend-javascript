c9899af02b8e3147d876bfd3020ee9da9d3f3dda
Sn0wF1re <mokayaclinton34@gmail.com>
2023-04-20 16:20:05 +0300

    Implement a class that returns a string or a number depending on how it's cast

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,18 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size === 'number') {
+      this._size = size;
+    }
+    if (typeof location === 'string') {
+      this._location = location;
+    }
+  }
+
+  toString() {
+    return this._location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+}
