5819fb5db4365d6f42e0d11544893504e448cd43
Sn0wF1re <mokayaclinton34@gmail.com>
2023-04-24 20:55:02 +0300

    Fix code to pass all tests

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,12 @@
 function cleanSet(set, startString) {
   const myArr = [];
-  set.forEach((value) => {
-    if (startString && value.startsWith(startString)) {
-      myArr.push(value.slice(startString.length));
-    }
-  });
+  if (startString && typeof startString === 'string') {
+    set.forEach((item) => {
+      if (item && item.startsWith(startString)) {
+        myArr.push(item.slice(startString.length));
+      }
+    });
+  }
   return myArr.join('-');
 }
 export default cleanSet;
