f8082474bdb1d9a275d16a4a73f461669d96aa66
Sn0wF1re <mokayaclinton34@gmail.com>
2023-04-24 22:13:26 +0300

    Fix code

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,8 @@
 function updateUniqueItems(map) {
-  for (const key of map.keys()) {
-    if (map.get(key) === 1) map.set(key, 100);
+  if (map instanceof Map) {
+    for (const key of map.keys()) {
+      if (map.get(key) === 1) map.set(key, 100);
+    }
   }
   return map;
 }
