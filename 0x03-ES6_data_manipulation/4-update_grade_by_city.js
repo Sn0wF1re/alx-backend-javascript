1d17ee0dc576530407b1ae989f02b12362229a79
Sn0wF1re <mokayaclinton34@gmail.com>
2023-04-25 00:00:12 +0300

    Fix code

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,5 @@
-import getStudentsByLocation from './2-get_students_by_loc';
-
 function updateStudentGradeByCity(listObjs, city, newGrades) {
-  const byLoc = getStudentsByLocation(listObjs, city);
+  const byLoc = listObjs.filter((student) => student.location === city);
   const updateGrades = byLoc.map(
     (student) => {
       for (const studentInfo of newGrades) {
